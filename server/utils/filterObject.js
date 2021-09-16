d16048267d844ae91e4f314fcb7edb59cf21bf89
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-09-16 13:20:33 -0700

    refactor(login-system-filter): change the parameter name, and change the if-conditional parameter

--- server/utils/filterObject.js
+++ server/utils/filterObject.js
@@ -1,8 +1,8 @@
-module.exports = (obj, ...notAllowedFields) => {
+module.exports = (obj, ...allowedFields) => {
   const newObj = {};
 
   Object.keys(obj).forEach((el) => {
-    if (!notAllowedFields.includes(el)) {
+    if (allowedFields.includes(el)) {
       newObj[el] = obj[el];
     }
   });
