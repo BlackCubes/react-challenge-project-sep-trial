25e59a0684055ad940f6ad9a6d63b2ca6b1c62ad
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-09-16 16:48:41 -0700

    refactor(login-system): refactor the login route

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,19 +1,9 @@
-const express = require('express');
+const express = require("express");
+const { authController } = require("../controllers");
+const { authValidation } = require("../validations");
 
 const router = express.Router();
 
-// login expects email/password
-// successful login returns email and a fake token (if we ever want to use it)
-router.post('/login', (req, res) => {
-  try {
-    if (!req.body || !req.body.email || !req.body.password) {
-      res.status(401).json({ success: false, error: 'Bad login information' });
-      return;
-    }
-    res.status(200).json({ success: true, email: req.body.email, token: '12345luggage' });
-  } catch (error) {
-    res.status(500).json({ success: false, error: 'Unknown error' });
-  }
-})
+router.post("/login", authValidation.login, authController.login);
 
 module.exports = router;
