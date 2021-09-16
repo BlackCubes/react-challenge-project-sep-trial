8bb7f3ead139c86685d9afdc3a3c1d06912db2ed
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-09-16 13:28:17 -0700

    feat(login-system-middleware): create an auth login middleware

---
+++ server/controllers/authController.js
@@ -0,0 +1,32 @@
+const { User } = require("../models");
+const { AppError } = require("../errors");
+const { catchAsync, filterObject, sanitize } = require("../utils");
+
+// login expects email/password
+// successful login returns email and a fake token (if we
+// evet want to use it)
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = sanitize(
+    filterObject(req.body, "email", "password")
+  );
+
+  if (!email || !password) {
+    return next(new AppError("Please provide an email and password!", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+  const correctPassword = await user.correctPassword(password, user.password);
+
+  if (!user || !correctPassword) {
+    return next(new AppError("Incorrect email or password!", 401));
+  }
+
+  // Remove password from potential output
+  user.password = undefined;
+
+  res.status(200).json({
+    success: true,
+    email,
+    token: "12345phpIsAwesome",
+  });
+});
