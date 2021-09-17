475d12a29ac68b7bfa9b1a845870a98055455345
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-09-16 20:45:13 -0700

    fix(error-handler): change the second parameter to be a request and not a response

--- server/errors/globalErrorHandler.js
+++ server/errors/globalErrorHandler.js
@@ -55,7 +55,7 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || "error";
 
   if (process.env.NODE_ENV === "development") {
-    sendErrorDev(err, res, res);
+    sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === "production") {
     let error = { ...err };
     error.message = err.message;
