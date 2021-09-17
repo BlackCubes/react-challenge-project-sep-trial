e4d14aa8c3d74a4f29ae1448d61aa45a74f71847
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-09-17 16:13:15 -0700

    feat(signup-system): add funtionality it resets the authSuccess state and redirects to login page upon success

--- application/src/components/signup/Signup.js
+++ application/src/components/signup/Signup.js
@@ -1,19 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
+import { useHistory } from "react-router-dom";
 import SignupForm from "./signup-form";
+import { finishAuthSuccess } from "../../redux/actions/authActions";
 
 const mapStateToProps = (state) => ({
   errorAuth: state.auth.error,
+  successAuth: state.auth.success,
 });
 
-const Signup = ({ errorAuth }) => (
-  <div className="main-body">
-    <h1 className="text-center">Signup</h1>
+const mapDispatchToProps = (dispatch) => ({
+  commenceFinishAuthSuccess: (success) => dispatch(finishAuthSuccess(success)),
+});
 
-    <div className="d-flex justify-content-center mt-5">
-      <SignupForm />
+const Signup = ({ commenceFinishAuthSuccess, errorAuth, successAuth }) => {
+  const history = useHistory();
+
+  useEffect(() => {
+    if (successAuth) {
+      commenceFinishAuthSuccess(false);
+      history.push("/login");
+    }
+  }, [successAuth]);
+
+  return (
+    <div className="main-body">
+      <h1 className="text-center">Signup</h1>
+
+      <div className="d-flex justify-content-center mt-5">
+        <SignupForm />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
-export default connect(mapStateToProps, null)(Signup);
+export default connect(mapStateToProps, mapDispatchToProps)(Signup);
