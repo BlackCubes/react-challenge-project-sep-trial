f1a76d7385b432e666cae8a55ab4d00d7a0e1a64
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-09-16 17:42:46 -0700

    feat(error-handler): create errors to unknown routes, and handle any global errors on the server

--- server/index.js
+++ server/index.js
@@ -1,29 +1,31 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
 
-const authRoutes = require('./routes/auth.routes');
-const routes = require('./routes/public.routes');
+const authRoutes = require("./routes/auth.routes");
+const routes = require("./routes/public.routes");
+const { AppError, globalErrorHandler } = require("./errors");
+
 const app = express();
 const port = 4000;
 
-require('dotenv').config();
+require("dotenv").config();
 
 // DATABASE SETUP
 // connect to db
 mongoose.connect(process.env.MONGODATABASE);
 // display message on connect
-mongoose.connection.on('connected', () => {
-  console.log('Connected to databse: ', process.env.MONGODATABASE);
+mongoose.connection.on("connected", () => {
+  console.log("Connected to databse: ", process.env.MONGODATABASE);
 });
 // display message on error
-mongoose.connection.on('error', (err) => {
-  console.log('Database error: ', err);
+mongoose.connection.on("error", (err) => {
+  console.log("Database error: ", err);
 });
 
 // logger
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // bodyparser
 app.use(bodyParser.json());
@@ -32,22 +34,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // CORS
 // This allows client applications from other domains use the API Server
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Expose-Headers', 'Authorization, refresh');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, refresh');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Expose-Headers", "Authorization, refresh");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization, refresh"
+  );
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
   next();
 });
 
 // use routes
-app.use('/api', authRoutes);
-app.use('/api', routes);
+app.use("/api", authRoutes);
+app.use("/api", routes);
 
 // test route
-app.get('/', (req, res) => {
-    console.log('Hi!');
-    res.send('Hi!');
-})
+app.get("/", (req, res) => {
+  console.log("Hi!");
+  res.send("Hi!");
+});
 
-app.listen(port, () => { console.log(`Listening on port ${port}`)});
+// Errors
+// --- unknown routes
+app.all("*", (req, res, next) =>
+  next(new AppError(`Could not find ${req.originalUrl} on this server!`, 404))
+);
+
+// --- global errors
+app.use(globalErrorHandler);
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
