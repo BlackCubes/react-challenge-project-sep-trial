177cdf6221240613727cb42238bfed4e77bd4cfc
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-09-16 21:13:45 -0700

    feat(signup-system-validation): create a validation for user signing up

--- server/validations/auth.validations.js
+++ server/validations/auth.validations.js
@@ -1,6 +1,16 @@
 const validatorHandler = require("./validatorHandler");
 const { catchAsync } = require("../utils");
 
+exports.signup = catchAsync(async (req, res, next) => {
+  const validationRule = {
+    email: "required|email|exist:User,email",
+    password: "required|string|min:8|max:60|confirmed|regexPassword",
+    password_confirmation: "required|string",
+  };
+
+  validatorHandler(req, next, validationRule);
+});
+
 exports.login = catchAsync(async (req, res, next) => {
   const validationRule = {
     email: "required|email",
