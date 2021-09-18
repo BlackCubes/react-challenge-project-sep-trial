21ebd14a353e73af47473b1886ea6c31a660ea06
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-09-17 17:52:14 -0700

    feat(login-component): redirect the user after successful login using redux and react-router-dom

--- application/src/components/login/Login.js
+++ application/src/components/login/Login.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { useHistory } from "react-router-dom";
 import LoginForm from "./login-form";
+import { finishAuthSuccess } from "../../redux/actions/authActions";
 import "./login.css";
 
-const Login = () => (
-  <div className="main-body">
-    <h1 className="text-center">Login Screen</h1>
-    <div className="d-flex justify-content-center mt-5">
-      <LoginForm />
-    </div>
-  </div>
-);
+const mapStateToProps = (state) => ({
+  errorAuth: state.auth.error,
+  successAuth: state.auth.success,
+});
 
-export default Login;
+const mapDispatchToProps = (dispatch) => ({
+  commenceFinishAuthSuccess: (success) => dispatch(finishAuthSuccess(success)),
+});
+
+const Login = ({ commenceFinishAuthSuccess, errorAuth, successAuth }) => {
+  const history = useHistory();
+
+  useEffect(() => {
+    if (successAuth) {
+      commenceFinishAuthSuccess(false);
+      history.replace("/view-orders");
+    }
+  }, [successAuth]);
+
+  return (
+    <div className="main-body">
+      <h1 className="text-center">Login Screen</h1>
+
+      <div className="d-flex justify-content-center mt-5">
+        <LoginForm />
+      </div>
+    </div>
+  );
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
