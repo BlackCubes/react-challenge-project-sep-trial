81466977acbc476b52cc55a1fad960bb69389175
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-09-21 00:09:05 -0700

    feat(auth-api): add setTimeouts

--- application/src/api/authAPI.js
+++ application/src/api/authAPI.js
@@ -7,33 +7,37 @@ const axiosInit = axios.create({
 });
 
 export const createUser = (email, password, password_confirmation, headers) =>
-  new Promise((resolve, reject) => {
-    try {
-      axiosInit
-        .post(
-          "/signup",
-          {
-            email,
-            password,
-            password_confirmation,
-          },
-          headers
-        )
-        .then((res) => resolve(res.data))
-        .catch((err) => reject(err.response));
-    } catch (err) {
-      reject("System error. Please try again later.");
-    }
-  });
+  new Promise((resolve, reject) =>
+    setTimeout(() => {
+      try {
+        axiosInit
+          .post(
+            "/signup",
+            {
+              email,
+              password,
+              password_confirmation,
+            },
+            headers
+          )
+          .then((res) => resolve(res.data))
+          .catch((err) => reject(err.response));
+      } catch (err) {
+        reject("System error. Please try again later.");
+      }
+    }, 1000)
+  );
 
 export const createLoginUser = (email, password, headers) =>
-  new Promise((resolve, reject) => {
-    try {
-      axiosInit
-        .post("/login", { email, password }, headers)
-        .then((res) => resolve(res.data))
-        .catch((err) => reject(err.response));
-    } catch (err) {
-      reject("System error. Please try again later.");
-    }
-  });
+  new Promise((resolve, reject) =>
+    setTimeout(() => {
+      try {
+        axiosInit
+          .post("/login", { email, password }, headers)
+          .then((res) => resolve(res.data))
+          .catch((err) => reject(err.response));
+      } catch (err) {
+        reject("System error. Please try again later.");
+      }
+    }, 1000)
+  );
