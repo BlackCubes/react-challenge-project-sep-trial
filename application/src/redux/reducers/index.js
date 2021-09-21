ecdf6373dc7097a41695761a9792c8577f3141ea
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-09-21 10:07:05 -0700

    feat(redux-persist): create persistor, persistReducer, and integrate React with PersistGate

--- application/src/redux/reducers/index.js
+++ application/src/redux/reducers/index.js
@@ -1,10 +1,19 @@
 import { combineReducers } from "redux";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import TempReducer from "./tempReducer";
 import authReducer from "./authReducer";
 import orderReducer from "./orderReducer";
 
-export default combineReducers({
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
   temp: TempReducer,
   auth: authReducer,
   order: orderReducer,
 });
+
+export default persistReducer(persistConfig, rootReducer);
