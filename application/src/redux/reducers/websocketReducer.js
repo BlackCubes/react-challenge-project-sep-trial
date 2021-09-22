1999e474c9e148956dc5dcbd8daac618ffba3c31
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-09-22 14:44:44 -0700

    refactor(connect-websocket-reducer): refactor return for CONNECT_WEBSOCKET, and add new cases for success and error CONNECT_WEBSOCKET

--- application/src/redux/reducers/websocketReducer.js
+++ application/src/redux/reducers/websocketReducer.js
@@ -1,16 +1,40 @@
 import {
   CONNECT_WEBSOCKET,
+  CONNECT_WEBSOCKET_ERROR,
+  CONNECT_WEBSOCKET_SUCCESS,
   MESSAGE_WEBSOCKET,
 } from "../constants/websocketTypes";
 
-const INITIAL_STATE = { socketId: "", message: "" };
+const INITIAL_STATE = {
+  loading: false,
+  loaded: false,
+  socketId: "",
+  message: "",
+  error: "",
+};
 
 const websocketReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case CONNECT_WEBSOCKET:
       return {
         ...state,
-        socketId: action.payload.socketId,
+        loading: true,
+      };
+    case CONNECT_WEBSOCKET_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        loaded: true,
+        socketId: action.result.socketId,
+        error: "",
+      };
+    case CONNECT_WEBSOCKET_ERROR:
+      return {
+        ...state,
+        loading: false,
+        loaded: false,
+        socketId: "",
+        error: action.error,
       };
     case MESSAGE_WEBSOCKET:
       return {
