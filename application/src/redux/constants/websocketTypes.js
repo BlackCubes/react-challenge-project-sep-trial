bcc8f08bdd776757e830bd2ec97739652964c27c
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-09-22 13:13:17 -0700

    feat(websocket-reduc-types): create three new constants

--- application/src/redux/constants/websocketTypes.js
+++ application/src/redux/constants/websocketTypes.js
@@ -1,2 +1,5 @@
+export const SUCCESS_WEBSOCKET = "websocket/success";
+export const ERROR_WEBSOCKET = "websocket/error";
+export const LISTEN_WEBSOCKET = "websocket/listen";
 export const CONNECT_WEBSOCKET = "websocket/connect";
 export const MESSAGE_WEBSOCKET = "websocket/message";
