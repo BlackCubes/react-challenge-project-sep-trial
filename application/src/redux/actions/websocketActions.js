d55a6a515ce2552c394a18df2745ec7f9521ba77
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-09-22 14:16:50 -0700

    refactor(websocket-redux-actions): change the action for connecting websocket to accomadate the websocketMiddleware

--- application/src/redux/actions/websocketActions.js
+++ application/src/redux/actions/websocketActions.js
@@ -1,12 +1,19 @@
 import {
   CONNECT_WEBSOCKET,
+  CONNECT_WEBSOCKET_ERROR,
+  CONNECT_WEBSOCKET_SUCCESS,
   MESSAGE_WEBSOCKET,
 } from "../constants/websocketTypes";
 
 // WEBSOCKET CONNECT
-export const websocketConnect = (socketId) => ({
-  type: CONNECT_WEBSOCKET,
-  payload: { socketId },
+export const websocketConnect = () => ({
+  type: "socket",
+  types: [
+    CONNECT_WEBSOCKET,
+    CONNECT_WEBSOCKET_SUCCESS,
+    CONNECT_WEBSOCKET_ERROR,
+  ],
+  promise: (socket) => socket.connect(),
 });
 
 // WEBSOCKET MESSAGE
