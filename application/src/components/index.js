a9e033b9ed6b63030712e2b80c3777864a789bdc
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-09-24 11:36:35 -0700

    feat(module-export): import/export nav components

--- application/src/components/index.js
+++ application/src/components/index.js
@@ -1,9 +1,18 @@
 import Login from "./login";
 import Main from "./main/main";
-import Nav from "./nav/nav";
+import { AuthNav, NormalNav } from "./nav";
 import OrderForm from "./order-form/OrderForm";
 import Signup from "./signup";
 import Template from "./common/template";
 import ViewOrders from "./view-orders/ViewOrders";
 
-export { Login, OrderForm, Main, Nav, Signup, Template, ViewOrders };
+export {
+  Login,
+  OrderForm,
+  Main,
+  AuthNav,
+  NormalNav,
+  Signup,
+  Template,
+  ViewOrders,
+};
