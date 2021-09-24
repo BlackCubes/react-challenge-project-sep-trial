82de8338b1cd9851a510c8ebfd261365e676d9cf
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-09-24 11:24:21 -0700

    fix(import-rename): change css import and rename component

--- application/src/components/nav/NormalNav.js
+++ application/src/components/nav/NormalNav.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import { Nav } from '../../components';
-import './template.css';
+import React from "react";
+import "./NormalNav.css";
 
-const Template = props => {
-    return (
-        <div className="bg-layer">
-            <div className="fg-layer">
-                <label className="logo">Bruce's Diner</label>
-                <Nav />
-                {props.children}
-            </div>
-        </div>
-    );
-}
+const NormalNav = (props) => {
+  return (
+    <div className="bg-layer">
+      <div className="fg-layer">
+        <label className="logo">Bruce's Diner</label>
+        {props.children}
+      </div>
+    </div>
+  );
+};
 
-export default Template;
+export default NormalNav;
